ba7ab9069cee596b501ac5af5be20df46536e0d3
Thomas Boulund Meng Hansen <thomasboulundmenghansen@Thomass-MacBook-Pro-3.local>
2023-09-06 11:11:23 +0200

    Added screenshots

--- test.js
+++ test.js
@@ -9,7 +9,9 @@ test("Subpage navigation", async t => {
         .expect(Selector("#welcome").innerText).eql("Welcome to the frontpage")
         .expect(Selector("body").getStyleProperty("background-color")).eql("rgb(240, 128, 128)")
         // Act
+        .takeScreenshot()
         .click(Selector("#subpage-link"))
+        .takeScreenshot()
         // Assert
         .expect(Selector("#welcome").innerText).eql("Welcome to the subpage")
         .expect(Selector("body").getStyleProperty("background-color")).eql("rgb(173, 216, 230)")
